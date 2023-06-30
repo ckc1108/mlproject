bfe0ceeaf738f7171531927295763e411b3ccca8
chirg contractor <ck.contractor99@gmail.com>
2023-06-30 17:17:40 -0400

    Model Trainer done git status

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,7 @@ from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 from src.exception import CustomException
 from src.logger import logging
+from src.components.model_trainer import ModelTrainer,ModelTrainerConfig
 
 from dataclasses import dataclass
 
@@ -54,8 +55,12 @@ class DataIngestion:
             raise CustomException(e,sys)
         
 if __name__=="__main__":
+
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    model_trainer=ModelTrainer()
+    print(model_trainer.initiate_model_trainder(train_arr,test_arr))
